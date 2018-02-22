54e8daf1cd8c6e317b57e34f7d9321465059bb24
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-22 02:43:31 -0800

    Create Timedifference.c

---
+++ Timedifference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("Enter the time 1:");
+    scanf("%d\t%d",&a,&b);
+    printf("Enter the time 2:");
+    scanf("%d\t%d",&c,&d);
+    e=a-c;
+    f=d-b;
+    printf("%d\t%d",e,f);
+    
+}
